993877eb55afdf5b6f450ff3ebf37844b7474416
Czekaj Tom <red57101@gmail.com>
2023-11-04 03:48:09 +0100

    :bug: Fix remaining numeric http status code

--- src/api/routes/v1/user.route.js
+++ src/api/routes/v1/user.route.js
@@ -1,17 +1,18 @@
 const {getUser, createUser} = require("../../controllers/user.controller");
 const userValidation = require("../../validations/user.validation");
-const {validate} = require("express-validation");
 const jwtAuth = require("../../middlewares/jwt.middleware");
+const {StatusCodes} = require("http-status-codes");
+const {validate} = require("express-validation");
 
 module.exports = (router) => {
     router.get("/user/:id",
         jwtAuth,
-        validate(userValidation.fetch, {context: false, statusCode: 400, keyByField: false}),
+        validate(userValidation.fetch, {context: false, statusCode: StatusCodes.BAD_REQUEST, keyByField: false}),
         async(req, res) => {
             await getUser(req, res);
         });
     router.post("/user",
-        validate(userValidation.create, {context: false, statusCode: 400, keyByField: false}),
+        validate(userValidation.create, {context: false, statusCode: StatusCodes.BAD_REQUEST, keyByField: false}),
         async(req, res) => {
             await createUser(req, res);
         });
