c405e4b8e3bea51ba146566c8987459f5ad39319
Czekaj Tom <red57101@gmail.com>
2023-12-04 15:59:31 +0100

    :rotating_light: Fix linting errors

--- src/api/middlewares/logger.middleware.js
+++ src/api/middlewares/logger.middleware.js
@@ -15,11 +15,11 @@ function getStatusColor(statusCode){
     }
 }
 
-function getCurrentTime() {
+function getCurrentTime(){
     const now = new Date();
-    const hours = String(now.getHours()).padStart(2, '0');
-    const minutes = String(now.getMinutes()).padStart(2, '0');
-    const seconds = String(now.getSeconds()).padStart(2, '0');
+    const hours = String(now.getHours()).padStart(2, "0");
+    const minutes = String(now.getMinutes()).padStart(2, "0");
+    const seconds = String(now.getSeconds()).padStart(2, "0");
     return `[${hours}:${minutes}:${seconds}]`;
 }
 
