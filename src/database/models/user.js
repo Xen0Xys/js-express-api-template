a6b64f4c1775306f3d5af4bfee223326a5390044
Czekaj Tom <red57101@gmail.com>
2023-12-05 09:47:42 +0100

    :art: Minor changes in sequelize models files

--- src/database/models/user.js
+++ src/database/models/user.js
@@ -1,15 +1,8 @@
 /*eslint-disable no-unused-vars*/
-const {
-    Model
-} = require("sequelize");
+const {Model} = require("sequelize");
 const {Joi} = require("sequelize-joi");
 module.exports = (sequelize, DataTypes) => {
     class User extends Model{
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
         static associate(models){
             models.User.belongsTo(models.Group, {
                 foreignKey: "groupId",
